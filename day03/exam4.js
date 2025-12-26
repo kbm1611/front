c57eb9a9437962ba41564679cb131607b9055649
kbm1611 <bm1611@naver.com>
2025-12-26 18:05:27 +0900

    251226practice4완료

--- day03/exam4.js
+++ day03/exam4.js
@@ -21,11 +21,20 @@ console.log(`result2 : ${radius*radius*3.14}`);
 // [지문3] prompt 함수로 두 실수를 입력받아서 앞 실수의 값이 뒤실수의 값의 비율% 계산하여 console탭에 출력하시오.
 let a = parseFloat(prompt(`실수1`));
 let b = parseFloat(prompt(`실수2`));
-console.log(`${a/b*100}%`);
+console.log(`result3 : ${a/b*100}%`);
+
+// 2] 비교연산자 : >초과 <미안 >=이상 <=이하 ==(값)같다 !=(값) 같지않다 ===(값과타입)같다 !==(값과타입)같지않다.
+// 비교 결과는 true or false
+
+// 3] 논리연산자 : 비교연산자 2개 이상일 때
+// && 이면서 이고 면서 그리고 모두, 모두 참이면 참, 초등학생이 사탕이랑 초콜릿 먹을래
+// || 이거나 거나 또는 하나라도, 하나라도 참이면 참, 초등학생이 사탕이나 초콜릿 먹을래
+//! : 부정 반대 not조건, true -> false, false -> true
+// 주의할점 : 50 < 점수 < 100 표현식이 없다, ---> 점수 > 50 && 점수 < 100
 
 // [지문4] prompt 함수로 정수를 입력받아 입력받은 값이 홀수이면 true / 짝수이면 false 로 console탭에 출력하시오.
 let c = parseInt(prompt(`정수1`));
-console.log(Boolean(c%2==1));
+console.log(Boolean(`result4 : ${c%2==1}`));
 
 // [지문5] prompt 함수로 정수를 입력받아 입력받은 값이 7의 배수이면 true / 아니면 false 로 console탭에 출력하시오.
 let d = parseInt(prompt(`정수2`));
@@ -34,7 +43,7 @@ console.log(Boolean(d%7==0));
 // [지문6] prompt 함수로 아이디 와 비밀번호를 입력받아서 (입력받은)아이디가 'admin' 이고 (입력받은)비밀번호가 1234 와 일치하면 true / 아니면 false 출력하시오.
 let id = prompt(`ID`);
 let pw = prompt(`PW`);
-console.log(Boolean(ID == "admin") && Boolean(PW == "1234"));
+console.log(Boolean(id == "admin") && Boolean(pw == "1234"));
 
 // [지문7] prompt 함수로 정수를 입력받아 입력받은 값이 홀수 이면서 7배수 이면 true / 아니면 false 로 console탭에 출력하시오.
 let e = parseInt(prompt(`정수3`));
@@ -43,7 +52,14 @@ console.log(Boolean(e%2==1) && Boolean(e%7==0));
 // [지문8] 1차점수 와 2차점수 prompt함수로 각 입력받아서 총점이 150점이상이면 '합격' 아니면 '불합격' HTML의 <h3> 에 출력하시오.
 let fir_score = parseFloat(prompt(`1차 점수`));
 let sec_score = parseFloat(prompt(`2차 점수`));
+total_score = fir_score + sec_score
+let result = total_score >= 150 ? "합격" : "불합격"
+console.log( result );
 
 // [지문9] 두 사람의 이름을 prompt함수로 각 입력받아서 만일 이름이 '유재석' 이면 뒤에 (방장) 이라고 이름 뒤에 붙이고 아니면 생략한다.  HTML의 <ol> 에 결과를 출력하시오.
-let human1 = prompt(`이름1`);
-let human2 = prompt(`이름2`);
+let name1 = prompt(`이름1`);
+let name2 = prompt(`이름2`);
+let result2 = name1=="유재석"?'${이름}(방장)' : '${이름}';
+let result3 = name2=="유재석"?'${이름}(방장)' : '${이름}';
+console.log( result2 );
+console.log( result3 );
---
+++ day03/practice4.js
@@ -0,0 +1,48 @@
+//문제1 : 삼각형 넓이 계산
+let 밑변 = Number(prompt("밑변"));
+let 높이 = Number(prompt("높이"));
+console.log(`삼각형 넓이 : ${밑변*높이/2}`);
+
+//문제2 : 섭씨를 화씨로 변환
+let 섭씨 = Number(prompt("섭씨 온도"));
+console.log(`화씨 온도 : ${(섭씨* 9 / 5) + 32}`);
+
+
+//문제3 : 나이 계산기
+let year = Number(prompt("태어난 년도"));
+console.log(`나이 : ${2025 - year}`);
+
+
+//문제4 : BMI 지수 계산
+let cm = Number(prompt("키"));
+let kg = Number(prompt("몸무게"));
+console.log(`BMI : ${kg/(cm/100*cm/100)}`);
+
+//문제5 : 관리자 확인
+let id = prompt("아이디");
+let pw = prompt("비밀번호");
+console.log(`result: ${id == "admin" || id == "admin@test.com" ? "관리자" : "일반 사용자"}`)
+
+//문제6 : 학점 변환기(HTML 출력)
+let score = Number(prompt("점수"));
+console.log(`result : ${score >= 90 ? 'A' : score >= 80 ? 'B' : score < 70 ? 'C' : 'C+'}`);
+
+//문제7 : 청년 할인 이벤트 대상 확인
+let age = Number(prompt("나이"));
+console.log(`result : ${age >= 20 && age <=29 ? "이벤트 대상입니다." : "이벤트 대상이 아닙니다."}`);
+
+//문제8 : '좋아요'카운터 만들기
+let likeCount = 3;
+likeCount++;
+console.log(`좋아요:${likeCount}`);
+
+//문제9 : 할 일 목록에 항목 추가하기
+let todoList = ['장보기', '과제하기'];
+let newItem = prompt("새로운 할 일");
+todoList.push(newItem);
+console.log(todoList);
+
+
+//문제10 : 대기열의 마지막 사람 확인하기
+let waitingList = ['김민준', '이서연', '박도윤', '최지우'];
+console.log(`마지막 대기자는 ${waitingList[waitingList.length-1]}입니다.`);
